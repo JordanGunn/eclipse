f66bf3c4dec574532acd72721eb7ad3d2c393339
Samsquatch <smay1299@gmail.com>
2023-12-04 14:48:58 -0800

    Added Delivery popup window
    Currently has no validation or functionality.

--- client/eclipse_gui.py
+++ client/eclipse_gui.py
@@ -3,8 +3,9 @@
 # Written by Sam May
 # ------------------------------------------------------------------------------
 
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk, filedialog, messagebox
+from tkcalendar import Calendar
 from multiprocessing import freeze_support
 import eclipse_config
 
@@ -16,7 +17,7 @@ import sys
 class ECLIPSE_GUI:
 
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = Tk()
         self.open_root_gui_on_top()
         self.set_icon(self.root)
         self.root.title("Eclipse Client")
@@ -55,22 +56,23 @@ class ECLIPSE_GUI:
             os.remove(icon_path)
 
     def main_gui(self):
-        navFrame = tk.Frame(self.root)
+        navFrame = Frame(self.root)
         navFrame.grid(row=0, column=0, padx=50)
 
         #Delivery Button
         ttk.Button(
             navFrame, 
             width=30, 
-            text="Delivery"
-            ).grid(row=0, column=0, padx=5, pady=5, ipady=10)
+            text="Delivery",
+            command=lambda: self.deliveryWindow()
+            ).grid(row=0, column=0, padx=10, pady=5, ipady=10)
         
         #LiDAR Button
         ttk.Button(
             navFrame, 
             width=30, 
             text="LiDAR"
-            ).grid(row=0, column=1, padx=5, pady=5, ipady=10)
+            ).grid(row=0, column=1, padx=10, pady=5, ipady=10)
         
         #NAS Box Button
         ttk.Button(
@@ -86,6 +88,116 @@ class ECLIPSE_GUI:
             text="NAS Box"
             ).grid(row=1, column=1, padx=5, pady=(0,5), ipady=10)
         
+        
+    def deliveryWindow(self):
+
+        popup=Toplevel()
+        self.open_popup_on_top_of_other_windows(popup)
+        popup.resizable(False, False)
+        self.set_icon(popup)
+        popup.title("Delivery")
+        popup.grab_set()
+
+        titleFrame = Frame(popup)
+        titleFrame.grid(row=0, column=0, pady=15)
+        ttk.Label(titleFrame, text="Delivery").grid(row=0, column=0, padx=30)
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # inputFrame - Select input directory
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        inputFrame = ttk.LabelFrame(popup, text="Input")
+        inputFrame.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+        
+        inputEntry = StringVar()
+        ttk.Entry(
+            inputFrame,
+            width=40,
+            textvariable=inputEntry
+        ).grid(row=0, column=0, padx=5)
+        ttk.Button(
+            inputFrame,
+            text="Browse to input\ndirectory",
+            command=lambda: self.path_select(inputEntry)
+        ).grid(row=0, column=1, pady=(0,5))
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Drive serial number - Serial number of the physical drive
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        driveFrame = ttk.LabelFrame(popup, text="Drive serial number")
+        driveFrame.grid(row=2, column=0, padx=10, pady=5, sticky=W)
+        
+        driveEntry = StringVar()
+        ttk.Entry(
+            driveFrame,
+            width=40,
+            textvariable=driveEntry
+        ).grid(row=0, column=0, padx=5, pady=(0,5))
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Receiver Name - Name of the GeoBC staff member
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        receiverFrame = ttk.LabelFrame(popup, text="Receiver name")
+        receiverFrame.grid(row=3, column=0, padx=10, pady=5, sticky=W)
+        
+        receiverEntry = StringVar()
+        ttk.Entry(
+            receiverFrame,
+            width=40,
+            textvariable=receiverEntry
+        ).grid(row=0, column=0, padx=5, pady=(0,5))
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Date received - The date the drive was received
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        dateFrame = ttk.LabelFrame(popup, text="Date received")
+        dateFrame.grid(row=4, column=0, padx=10, pady=5, sticky=W)
+        
+        calendar = Calendar(dateFrame, selectmode = 'day')
+        calendar.grid(row=0, column=0, padx=5, pady=(0,5))
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Comments - Any additional comments from the user
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        commentFrame = ttk.LabelFrame(popup, text="Comments")
+        commentFrame.grid(row=5, column=0, padx=10, pady=5, sticky=W)
+        
+        commentEntry = StringVar()
+        ttk.Entry(
+            commentFrame,
+            width=55,
+            textvariable=commentEntry
+        ).grid(row=0, column=0, padx=5, pady=(0,5))
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Submit Button - Saves and validates above info and sends to DB
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        submitFrame = ttk.Frame(popup)
+        submitFrame.grid(row=6, column=0, padx=10, pady=5)
+
+        ttk.Button(
+            submitFrame,
+            text="Submit",
+            command=lambda: self.deliveryValidation(inputEntry, driveEntry, receiverEntry, calendar, commentEntry)
+        ).grid(row=0, column=0)
+
+
+    def deliveryValidation(self, inputEntry, driveEntry, receiverEntry, calendar, commentEntry):
+        inputPath = inputEntry.get()
+        driveSerial = driveEntry.get()
+        receiverName = receiverEntry.get()
+        dateReceived = calendar.get_date()
+        comments = commentEntry.get()
+
+        
+        
+    def path_select(self, pathSV):
+        """
+        Opens 'select folder' windows dialog,
+        adds the selected folder to the relevant entrybox.
+        """
+        dirPath = filedialog.askdirectory(initialdir="/")
+        pathSV.set(dirPath)
+
 def main():
     gui = ECLIPSE_GUI()
     gui.root.mainloop()
